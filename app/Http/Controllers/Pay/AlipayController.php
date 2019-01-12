9ae0b85b951938ada4cf1f201b6e497fe7e90827
mazile <2231185856@qq.com>
2019-01-12 16:14:48 +0800

    创建alipay

---
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Pay;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use GuzzleHttp\Client;
+
+class AlipayController extends Controller
+{
+    
+}

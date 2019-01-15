d3b37ddecea6e2b45fa0130bcf613d75ac0e4396
mazile <2231185856@qq.com>
2019-01-15 16:44:35 +0800

    异步

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -300,5 +300,17 @@ class AlipayController extends Controller
 
         return $result;
     }
+    /**
+     * 处理订单逻辑 更新订单 支付状态 更新订单支付金额 支付时间
+     * @param $data
+     */
+    public function dealOrder($data)
+    {
+
+
+        //加积分
+
+        //减库存
+    }
 
 }

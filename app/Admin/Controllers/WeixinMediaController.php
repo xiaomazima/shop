bd26a5b06ee432760547cb45e787641da4d3c78a
root <root@localhost.localdomain>
2019-02-21 10:15:16 +0800

    图片1

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -89,7 +89,7 @@ class WeixinMediaController extends Controller
         $grid->format('Format');
         $grid->msg_id('Msg id');
         $grid->local_file_name('Local file name')->display(function($aaa){
-            return '<img src="https://xmz.rps0228.cn/wx/images/'.$aaa.'">';
+            return '<img src="https://xmz.rps0228.cn/wx/images/'.$aaa.'" style="width:100px;height=100px">';
         });
         $grid->local_file_path('Local file path');
 

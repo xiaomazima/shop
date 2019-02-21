5384a817e2d8d6658804d76d23b4201e787b08f2
mazile <2231185856@qq.com>
2019-02-21 09:38:41 +0800

    下载图片素材11

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -88,7 +88,9 @@ class WeixinMediaController extends Controller
         $grid->media_id('Media id');
         $grid->format('Format');
         $grid->msg_id('Msg id');
-        $grid->local_file_name('Local file name');
+        $grid->local_file_name('Local file name')->display(function($aaa){
+            return "";
+        });
         $grid->local_file_path('Local file path');
 
         return $grid;

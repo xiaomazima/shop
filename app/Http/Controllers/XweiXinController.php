93b0e14f7cece319cb495ecd654becad5f8247f8
mazile <2231185856@qq.com>
2019-03-08 01:44:40 +0000

    考试

--- app/Http/Controllers/XweiXinController.php
+++ app/Http/Controllers/XweiXinController.php
@@ -44,17 +44,36 @@ class XweixinController extends Controller
 
             //记录日志
             $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
-            file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
+            file_put_contents('logs/event.log',$log_str,FILE_APPEND);
 
 
 
             $event = $xml->Event;                       //事件类型
-        var_dump($xml);echo '<hr>';die;
+//        var_dump($xml);echo '<hr>';die;
             $openid = $xml->FromUserName;               //用户openid
+            //处理用户发送信息
+            if(isset($xml->MsgType)){
+                if($xml->MsgType=='text'){ //用户发送文本消息
+                    $xml->Content;
+                    $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好啊！欢迎来到小麻子公众号]]></Content></xml>';
+                    echo $xml_response;
+
+                }
 
             if($event=="subscribe"){
+                $sub_time = $xml->CreateTime;               //扫码关注时间
+
+
+                echo 'openid: '.$openid;echo '</br>';
+                echo '$ksub_time: ' . $sub_time;
+
+                //获取用户信息
+//                $user_info = $this->getUserInfo($openid);
+//                echo '<pre>';print_r($user_info);echo '</pre>';
+                 }
 
             }
+
 
         }
 
@@ -67,7 +86,7 @@ class XweixinController extends Controller
 
     //获取用户信息
     public function WxUser($openid){
-        $openid='wxe355c6cac71fd488';
+//        $openid='wxe355c6cac71fd488';
         $access_token=$this->Accesstoken();
         $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $data = json_decode(file_get_contents($url),true);
